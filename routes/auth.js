4a967a7076cb5f74521064e439d89f34b90f0a0c
saeed <saeedelnahas36@gmail.com>
2022-02-15 21:25:07 +0200

    update

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/authentication')
-const {
-    register,
-    login,
-    getAllUsers,
-    getSingleUser,
-    updateUser,
-    deleteUser
+const { 
+    register, 
+    login, 
+    getAllUsers, 
+    getSingleUser, 
+    updateUser, 
+    deleteUser 
 } = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/', auth, getAllUsers);
-router.get('/:id', auth, getSingleUser);
-router.patch('/updateuser/:id', auth, updateUser);
+router.get('/', getAllUsers);
+router.get('/:id', getSingleUser);
+router.patch('/updateuser/:id', updateUser);
 //router.patch('/updatepassword/:id', updateUserPassword);
-router.delete('/:id', auth, deleteUser);
+router.delete('/:id', deleteUser);
 
 
 module.exports = router;

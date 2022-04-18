ab8453e9eb06a797d6ba1a510367ec8dee0ef3b8
saeed <saeedelnahas36@gmail.com>
2022-04-18 14:28:59 +0200

    first commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 
-//signup
+//signup register
 const register = async (req, res) => {
     const user = await User.create({ ...req.body });
     const token = user.createJWT()

b425bcd205098ed3916d03d3e37133e4ba48d911
saeed <saeedelnahas36@gmail.com>
2022-05-12 15:54:09 +0200

    first commit

--- models/Beach.js
+++ models/Beach.js
@@ -4,31 +4,31 @@ const beachSchema = new mongoose.Schema(
     {
         name: {
             type: String,
-            required: [true, 'Please provide beach name'],
+            //required: [true, 'Please provide beach name'],
         },
         photo: {
             type: String,
-            required: [true, 'Please provide beach photo'],
+            //required: [true, 'Please provide beach photo'],
         },
         location_map: {
             type: String,
-            required: [true, 'Please provide beach location_map'],
+            //required: [true, 'Please provide beach location_map'],
         },
         location: {
             type: String,
-            required: [true, 'Please provide beach location'],
+            //required: [true, 'Please provide beach location'],
         },
         link: {
             type: String,
-            required: [true, 'Please provide beach link'],
+            //required: [true, 'Please provide beach link'],
         },
         gallery: {
             type: Array,
-            required: [true, 'Please provide beach gallery'],
+            //required: [true, 'Please provide beach gallery'],
         },
         information: {
             type: String,
-            required: [true, 'Please provide beach information'],
+            //required: [true, 'Please provide beach information'],
         }
     },
     { timestamps: true }

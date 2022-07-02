acaf1634bb986aa72f2d05a49c85e0db00f5a321
saeed <saeedelnahas36@gmail.com>
2022-07-02 10:03:18 +0200

    make it better

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ const start = async () => {
         const io=require("socket.io")(server,{
             pingTimeout: 60000,
             cors: {
-            origin: "http://localhost:3000",
+            origin: "*",
             },
         })
 

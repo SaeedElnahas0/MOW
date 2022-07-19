613c510ff94ddb78141117dd21d22b5ee425437a
saeed <saeedelnahas36@gmail.com>
2022-07-19 11:01:23 +0200

    make it better

--- models/Form.js
+++ models/Form.js
@@ -5,7 +5,6 @@ const formSchema = new mongoose.Schema(
         user: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'User',
-            required: true
         },
         age: {
             type: Number
